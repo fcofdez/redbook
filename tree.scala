b221e9ae0cdc7b0b3f611d327a40e90e5f0a80ad
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2016-06-01 18:03:19 +0200

    Adding tree exercises.

--- tree.scala
+++ tree.scala
@@ -9,3 +9,23 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 //Depth
 //Map
 //Fold
+
+def size[A](tree: Tree[A]): Int = tree match {
+  case Leaf(_) => 1
+  case Branch(l, r) => size(l) + size(r) + 1
+}
+
+def max[Int](tree: Tree[Int]): Int = tree match {
+  case Leaf(a) => a
+  case Branch(l, r) => max(l) max max(r)
+}
+
+def depth[A](tree: Tree[A]): Int = tree match {
+  case Leaf(a) => 1
+  case Branch(l, r) => (depth(l) max depth(r)) + 1
+}
+
+def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = {
+  case Leaf(a) => Leaf(f(a))
+  case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+}
