2bb021f0b3ca91e8e7e08137c8c59322b818aa23
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2016-06-07 00:35:15 +0200

    Adding variance exercise.

--- error.scala
+++ error.scala
@@ -2,6 +2,17 @@ package errors
 
 import scala.{Option => _, Either => _, _}
 
+object exercise {
+  def mean(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+}
+
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
     case Some(get) => Some(f(get))
