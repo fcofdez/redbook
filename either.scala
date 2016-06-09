4e393843cc0c1d9eeeaf76ccbb32abb0a8167830
Francisco Fernandez <francisco.fernandez.castano@gmail.com>
2016-06-09 17:45:22 +0200

    Adding sequence and traverse to Either.

--- either.scala
+++ either.scala
@@ -2,18 +2,18 @@ package errors
 trait Either[+E, +A] {
 
   def map[B](f: A => B): Either[E, B] = this match {
-    case Left(_) => this
+    case Left(e) => Left(e)
     case Right(v) => Right(f(v))
   }
 
-  def flatMap[EE >: E, b](f: A => Either[EE, B]): Either[EE, B] = this match {
-    case Left(_) => this
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
     case Right(v) => f(v)
   }
 
   def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-    case Left(_) => b
-    case Right(_) => this
+    case Left(e) => Left(e)
+    case Right(v) => Right(v)
   }
 
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
@@ -26,3 +26,13 @@ trait Either[+E, +A] {
 
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
+
+def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+  case Nil => Right(Nil)
+  case h :: t => h flatMap(hh => sequence(t) map (hh :: _))
+}
+
+def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+  case Nil => Right(Nil)
+  case h :: t => f(h) flatMap(hh => traverse(t)(f) map (hh :: _))
+}
